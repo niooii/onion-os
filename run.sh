4f57891c4a8907f10b605a39ffd71073bd5f5a3a
Hewitt <wangh3@bxscience.edu>
2024-12-03 21:22:10 -0500

    cool scripts

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
+BUILDDIR="build"
+
+if !([ -d "$BUILDDIR" ];) then
+  echo "$BUILDDIR directory does not exist. Try running ./build.sh first"
+  exit 1
+fi
+
 qemu-system-x86_64 -drive if=pflash,format=raw,readonly=on,file=/usr/share/ovmf/x64/OVMF.4m.fd -drive format=raw,file=fat:rw:./build/ -net none
