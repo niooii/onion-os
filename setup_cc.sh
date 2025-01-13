3d399b49e7d187517635cc8bf23da0217f852738
niooii <onioniwonpounoin@gmail.com>
2025-01-13 02:39:34 -0500

    fix setup cross compiler script

--- setup_cc.sh
+++ setup_cc.sh
@@ -1,42 +1,46 @@
 export PREFIX="/usr/local/i386elfcc"
 TARGET=i386-elf
 export PATH="$PREFIX/bin:$PATH"
-
 CURRDIR=$(pwd)
 
+sudo mkdir -p "$PREFIX"
+sudo chown -R $USER:$USER "$PREFIX"
 mkdir -p ./tmp/src
 
+# binutils
 if ! [ -f "$PREFIX/bin/i386-elf-ld" ]; then
-  cd ./tmp/src
-  sudo rm -rf ./binutils-build
-  curl -O http://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.gz
-  tar xf binutils-2.39.tar.gz
-  mkdir binutils-build
-  cd binutils-build
-  ../binutils-2.39/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX 2>&1 | tee configure.log
-  sudo make -j 8 all install 2>&1 | tee make.log
+  cd ./tmp/src || exit 1
+  rm -rf ./binutils-build
+  curl -O http://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.gz || exit 1
+  tar xf binutils-2.39.tar.gz || exit 1
+  mkdir binutils-build && cd binutils-build || exit 1
+  ../binutils-2.39/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX || exit 1
+  make -j 8 || exit 1
+  sudo make install || exit 1
+  sudo chown -R $USER:$USER "$PREFIX"
 fi
 
+# gcc
 if ! [ -f "$PREFIX/bin/i386-elf-gcc" ]; then
-  cd "$CURRDIR"
-  cd ./tmp/src
-  sudo rm -rf ./gcc-build
-  curl -O https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.gz
-  tar xf gcc-12.2.0.tar.gz
-  mkdir gcc-build
-  cd gcc-build
-  echo Configure: . . . . . . .
-  ../gcc-12.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-libssp --enable-language=c,c++ --without-headers
-  echo MAKE ALL-GCC:
-  sudo make -j 8 all-gcc
-  echo MAKE ALL-TARGET-LIBGCC:
-  sudo make -j 8 all-target-libgcc
-  echo MAKE INSTALL-GCC:
-  sudo make -j 8 install-gcc
-  echo MAKE INSTALL-TARGET-LIBGCC:
-  sudo make -j 8 install-target-libgcc
+  cd "$CURRDIR" || exit 1
+  cd ./tmp/src || exit 1
+  rm -rf ./gcc-build
+  curl -O https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.gz || exit 1
+  tar xf gcc-12.2.0.tar.gz || exit 1
+  mkdir gcc-build && cd gcc-build || exit 1
+  
+  ../gcc-12.2.0/configure --target=$TARGET \
+    --prefix="$PREFIX" \
+    --disable-nls \
+    --disable-libssp \
+    --enable-languages=c,c++ \
+    --without-headers || exit 1
+    
+  make -j 8 all-gcc || exit 1
+  sudo make install-gcc || exit 1
+  make -j 8 all-target-libgcc || exit 1
+  sudo make install-target-libgcc || exit 1
+  sudo chown -R $USER:$USER "$PREFIX"
 fi
 
-echo WE GOOD:
-ls "$PREFIX"/bin
-
+ls "$PREFIX/bin"
