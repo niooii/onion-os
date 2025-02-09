81624cdbe2f0721357764d6d7401e364415d1650
Hewitt <wangh3@bxscience.edu>
2025-02-08 20:19:13 -0500

    a

--- loader/boot.s
+++ loader/boot.s
@@ -102,61 +102,47 @@ start_protected_mode:
 ; HATE. LET ME TELL YOU HOW MUCH I'VE COME TO HATE YOU, SINCE I'VE BEGAN TO LIVE.
 ata_lba_read:
     mov ebx, eax,
-    ; Send the highest 8 bits of the lba to hard disk controller
     shr eax, 24
-    or eax, 0xE0 ; Select the  master drive
+    or eax, 0xE0 
     mov dx, 0x1F6
     out dx, al
-    ; Finished sending the highest 8 bits of the lba
 
-    ; Send the total sectors to read
     mov eax, ecx
     mov dx, 0x1F2
     out dx, al
-    ; Finished sending the total sectors to read
 
-    ; Send more bits of the LBA
-    mov eax, ebx ; Restore the backup LBA
+    mov eax, ebx
     mov dx, 0x1F3
     out dx, al
-    ; Finished sending more bits of the LBA
 
-    ; Send more bits of the LBA
     mov dx, 0x1F4
-    mov eax, ebx ; Restore the backup LBA
+    mov eax, ebx
     shr eax, 8
     out dx, al
-    ; Finished sending more bits of the LBA
 
-    ; Send upper 16 bits of the LBA
     mov dx, 0x1F5
-    mov eax, ebx ; Restore the backup LBA
+    mov eax, ebx 
     shr eax, 16
     out dx, al
-    ; Finished sending upper 16 bits of the LBA
 
     mov dx, 0x1f7
     mov al, 0x20
     out dx, al
 
-    ; Read all sectors into memory
 .next_sector:
     push ecx
 
-; Checking if we need to read
 .try_again:
     mov dx, 0x1f7
     in al, dx
     test al, 8
     jz .try_again
 
-; We need to read 256 words at a time
     mov ecx, 256
     mov dx, 0x1F0
     rep insw
     pop ecx
     loop .next_sector
-    ; End of reading sectors into memory
     ret
 
 times 510-($-$$) db 0
