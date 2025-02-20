c5af4bab653c4abc09d5b065b0c1877effa5e82a
niooii <onioniwonpounoin@gmail.com>
2025-02-20 02:33:24 -0500

    fix page frame logic

--- C/init.c
+++ C/init.c
@@ -11,16 +11,19 @@ void kinit(uint32_t magic, struct multiboot_info* mbi)
     vga_cset(true);
 
     if (magic != 0x2BADB002) {
-        vga_println("OS was not booted by a multiboot 1 compliant bootloader, stopping.");
+        vga_println("OS was not booted by a multiboot 1 compliant bootloader, stop.");
         return;
     }
 
     if (!(mbi->flags & MULTIBOOT_INFO_MEM_MAP)) {
-        vga_println("Failed to obtain memory map, stopping.");
+        vga_println("Failed to obtain memory map, stop.");
         return;
     }
+
+    vga_println("Init memory.");
     if (!pmm_init(mbi))
         return;
+    vga_println("Finish.");
     // framebuffer stuff later
 
     idt_init();
--- C/mm/pmm.c
+++ C/mm/pmm.c
@@ -3,6 +3,7 @@
 
 #include "pmm.h"
 
+// Must be a power of 2
 #define FRAME_SIZE     0x1000
 #define FRAME_FREE_BIT 1
 #define FRAME_USED_BIT 0
@@ -31,14 +32,20 @@ extern char       kernel_end[];
 
 typedef multiboot_memory_map_t mm_entry;
 
-/*
- * FATAL! this entire function is wrong lol
- * instead of incrementing, (beause multiboot does not guarentee each entry
- * describes sequential and contiguous memory relative to the previous), we
- * need to directly calculate the offset in the bitmap, and memset from there. the bitmap
- * is now guarenteed to contain everything. (BUT THERE MAY BE GAPS after the iterations,
- * these must be accounted for by setting those pages to FRAME_BIT_USED)
- */
+// Status must be either FRAME_FREE_BIT or FRAME_USED_BIT
+// frame num 0 corresponds to the first bit in the bitmap
+void set_frame_bit(uint64_t frame_num, int status)
+{
+    uint64_t i = frame_num >> 6;
+    pmm.bitmap[i] |= ((uint64_t)status << (frame_num % 64));
+}
+
+// Status must be either FRAME_FREE_BIT or FRAME_USED_BIT
+// Start and end inclusive
+void set_frame_bits(uint64_t start_frame, uint64_t end_frame, int status)
+{
+}
+
 bool pmm_init(multiboot_info_t* mbi)
 {
     /*
@@ -48,55 +55,46 @@ bool pmm_init(multiboot_info_t* mbi)
      * If the rest of the free region where the kernel is isn't large enough?
      * handle later
      */
-    unsigned char* bm = (unsigned char*)(&kernel_end);
-    pmm.bitmap        = (uint64_t*)bm;
+    uint64_t* bm = (uint64_t*)(&kernel_end);
+    pmm.bitmap   = bm;
 
-    // The leftover bits from the last iteration of the loop
-    // TODO! unused for now figure out nuanced implementation details
-    uint32_t  prev_leftover = 0;
-    bool      first         = true;
-    mm_entry* entry         = (mm_entry*)mbi->mmap_addr;
-    size_t    mmap_end      = mbi->mmap_addr + mbi->mmap_length;
+    size_t mmap_end = mbi->mmap_addr + mbi->mmap_length;
+    // End of memory (addr) according to the mem map entries
+    uint64_t mem_end = 0;
 
     // BUG! does this miss/skip an entry by any chance??
     // i just fixed it but idk maybe its missing more
-    while ((size_t)entry < mmap_end) {
+    // first run to find end address
+    for (mm_entry* e = (mm_entry*)mbi->mmap_addr; (size_t)e < mmap_end;
+         e           = (mm_entry*)((size_t)e + e->size + sizeof(e->size))) {
+        uint64_t end = ALIGN_DOWN(e->addr + e->len, FRAME_SIZE);
+        // find the end address
+        if (end > mem_end)
+            mem_end = end;
+    }
+    // frame size trailing zeroes
+    int ftz = __builtin_ctz(FRAME_SIZE);
+
+    uint64_t num_frames = mem_end >> ftz;
+    // divide by 64 bc 64 frames can be stored in 1 uint64_t (and also round up)
+    uint64_t  bm_size = (num_frames + 63) >> 6;
+    uint64_t* bm_end  = bm + bm_size;
+    memset(bm, FRAME_USED_BIT, (bm_end - bm) * sizeof(uint64_t));
+    // second run to set bits
+    for (mm_entry* e = (mm_entry*)mbi->mmap_addr; (size_t)e < mmap_end;
+         e           = (mm_entry*)((size_t)e + e->size + sizeof(e->size))) {
         // Start and end are aligned to frame boundaries
-        uint64_t start = ALIGN_UP(entry->addr, FRAME_SIZE);
-        uint64_t end   = ALIGN_DOWN(start + entry->len, FRAME_SIZE);
+        uint64_t start = ALIGN_UP(e->addr, FRAME_SIZE);
+        uint64_t end   = ALIGN_DOWN(start + e->len, FRAME_SIZE);
 
-        // too small, discard
-        if (end <= start)
-            goto next_entry;
+        // either too small for our needs or not a good type
+        if (end <= start || e->type != MULTIBOOT_MEMORY_AVAILABLE)
+            continue;
 
-        if (first) {
-            first          = false;
-            pmm.start_addr = start;
-        }
+        uint64_t start_frame = start >> ftz;
+        uint64_t last_frame  = (end >> ftz) - 1;
 
-        uint32_t num_frames = (end - start) >> __builtin_ctz(FRAME_SIZE);
-        // divide by 8 bc 8 frames can be stored in 1 bit
-        uint32_t bytes_to_use = num_frames >> 3;
-
-        if (prev_leftover == 0) {
-            uint32_t leftover = num_frames % 8;
-
-            int status =
-                entry->type == MULTIBOOT_MEMORY_AVAILABLE ? FRAME_FREE_BIT : FRAME_USED_BIT;
-
-            // TODO! account for carry
-            memset(bm, status, bytes_to_use);
-            // account for carry here with some bit magic
-            bm += bytes_to_use;
-            prev_leftover = leftover;
-        } else {
-        }
-
-        pmm.total_frames += num_frames;
-
-    // TODO!
-    next_entry:
-        entry = (mm_entry*)((size_t)entry + entry->size + sizeof(entry->size));
+        set_frame_bits(start_frame, last_frame, FRAME_FREE_BIT);
     }
 
     // TODO! set the frames occupied by the frame bitmap to used
